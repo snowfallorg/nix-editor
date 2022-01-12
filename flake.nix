fb05d881589116d6bd09c5ff373887a862652123
Victor Fuentes <vmfuentes64@gmail.com>
2022-01-12 00:22:42 -0500

    Add flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Nix configuration command line editor";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, naersk }: 
+  flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        naersk-lib = naersk.lib."${system}";
+      in
+  {
+    packages.nixeditor = naersk-lib.buildPackage {
+      pname = "nix-editor";
+      root = ./.;
+      };
+
+    defaultPackage = self.packages.${system}.nixeditor;
+
+    devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ rustc cargo ];
+        };
+  });
+}
