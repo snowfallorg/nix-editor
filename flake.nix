a72c7d695d5568fe19ff34d161a22c716ffbdc07
holly <oliviafloof@disroot.org>
2026-02-26 14:19:55 +0000

    remove flake-utils

--- flake.nix
+++ flake.nix
@@ -3,42 +3,35 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, ... }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        name = "nix-editor";
-      in
-      rec
-      {
-        packages.${name} = pkgs.callPackage ./default.nix { };
-
-        # `nix build`
-        defaultPackage = packages.${name}; # legacy
-        packages.default = packages.${name};
-
-        # `nix run`
-        apps.${name} = utils.lib.mkApp {
-          inherit name;
-          drv = packages.${name};
-        };
-        defaultApp = apps.${name};
-
-        checks = self.packages.${system};
-        hydraJobs = self.packages.${system};
-
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            rust-analyzer
-            rustc
-            rustfmt
-            cargo
-            cargo-tarpaulin
-            clippy
-          ];
-        };
-      });
+  outputs =
+    {
+      nixpkgs,
+      ...
+    }:
+    let
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+          system: f nixpkgs.legacyPackages.${system} system
+        );
+    in
+    {
+      packages = forAllSystems (pkgs: _: { default = pkgs.callPackage ./default.nix { }; });
+      devShells = forAllSystems (
+        pkgs: _: {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              rust-analyzer
+              rustc
+              rustfmt
+              cargo
+              cargo-tarpaulin
+              clippy
+            ];
+          };
+        }
+      );
+    };
 }
